0589fcb2edc4c558dfd050cbb892ab904aa2cfa7
Ddorcenat <157091483+Ddorcenat@users.noreply.github.com>
2025-09-27 21:54:31 -0600

    Change

---
+++ ponder/dom-basic.js
@@ -0,0 +1,11 @@
+const newP = document.createElement("p");
+newP.textContent= "This is a new paragraph added by javaScript"
+document.body.append(newP)
+
+const NewaImg = Document.createElement("img");
+NewImg.src = "";
+NewImg.setAttribute("alt", "Random image")
+
+document.body.append(newImg);
+
+const myList = ["ONE", "Two", "Three"];
